24f275bf4838b67ba47fd04da0ed0817fd60d7f5
vishal <vishalpalode9899@gmail.com>
2022-01-28 00:37:58 +0530

    LargestAmongThreeNum

--- BasicCoreProgram/BasicPrograms.cs
+++ BasicCoreProgram/BasicPrograms.cs
@@ -8,14 +8,31 @@ namespace BasicCoreProgram
 {
     internal class BasicPrograms
     {
-        public void checkEvenOrOdd()
+        public void checkLargestNumber()
         {
-            Console.WriteLine("Enter a Number : ");
-            int numb = Convert.ToInt32(Console.ReadLine());
-            if (numb % 2 == 0)
-                Console.WriteLine(numb + " is Even Number");
+            Console.WriteLine("Enter first number: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number: ");
+            int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNum > secondNum && firstNum > thirdNum)
+            {
+                Console.WriteLine(firstNum + " is the largest among entered three numbers");
+            }
+            else if (secondNum > firstNum && secondNum > thirdNum)
+            {
+                Console.WriteLine(secondNum + " is the largest among entered three numbers");
+            }
+            else if (thirdNum > firstNum && thirdNum > secondNum)
+            {
+                Console.WriteLine(thirdNum + " is the largest among entered three numbers");
+            }
             else
-                Console.WriteLine(numb + " is Odd Number");
+            {
+                Console.WriteLine("All three entered  numbers are equal");
+            }
         }
     }
 }
